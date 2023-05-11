8ef61e719763340e7e28d54ecddcad2a94390984
Jordi Calles <jordicalles1989@gmail.com>
2023-05-11 20:23:38 +0200

    Add src index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-export const a = 3;
+import * as dotenv from "dotenv";
+import express from "express";
+import chalk from "chalk";
+
+dotenv.config();
+const port = process.env.PORT ?? 4006;
+
+const app = express();
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Listening on ${chalk.green(`http://localhost:${port}`)}`);
+});
